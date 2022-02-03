fea4ebe6d41e3e8702e8f8f776bac9f9aaa43ab6
phj-999 <865779043@qq.com>
2022-02-03 15:48:56 +0800

    异步组件

--- vue3+ts/src/main.js
+++ vue3+ts/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 //import App from './插槽/普通插槽/App.vue';
 //import App from './插槽/具名插槽/App.vue';
 //import App from './插槽/作用域插槽使用/App.vue';
-import App from './动态组件的使用/App.vue';
+//import App from './动态组件的使用/App.vue';
+import App from './异步组件的使用/App.vue';
 
 createApp(App).mount('#app')
