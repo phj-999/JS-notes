ba6e7866fb21bfecfb5883110f3c12e8196fda6d
phj-999 <865779043@qq.com>
2022-02-03 11:57:29 +0800

    作用域插槽使用

--- vue3+ts/src/main.js
+++ vue3+ts/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 //import App from './provide和inject的使用/App.vue'
 //import App from './全局事件总线mitt库/App.vue';
 //import App from './插槽/普通插槽/App.vue';
-import App from './插槽/具名插槽/App.vue';
+//import App from './插槽/具名插槽/App.vue';
+import App from './插槽/作用域插槽使用/App.vue';
 
 createApp(App).mount('#app')
