1bc0ebac7a3635eb76e1e852bcfbad1614a48b0e
phj-999 <865779043@qq.com>
2022-02-04 10:53:13 +0800

    CompositionAPI-setup+reactive

---
+++ vue3+ts/vue3的Mixin和CompositionAPI/CompositionAPI/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+ 
+</template>
+
+<script>
+ 
+export default {
+ 
+  components: {
+ 
+  }
+}
+</script>
+
+<style>
+ 
+</style>
---
+++ vue3+ts/vue3的Mixin和CompositionAPI/CompositionAPI/src/响应式reactive.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{state.counter}}</div>
+</template>
+
+<script>
+export default {
+setup(props) {
+    const state = reactive({
+        counter:100
+    })
+
+    const increment=()=>{
+        state.counter++
+    }
+    return{
+        state
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
