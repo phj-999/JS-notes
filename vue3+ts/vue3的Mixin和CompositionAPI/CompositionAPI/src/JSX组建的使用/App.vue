5f353718796c0d190766c69c76f164205b8e8668
phj-999 <865779043@qq.com>
2022-02-05 16:38:25 +0800

    jsx组件的使用

---
+++ vue3+ts/vue3的Mixin和CompositionAPI/CompositionAPI/src/JSX组建的使用/App.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+
+<script>
+import Hello from './Hello.vue';
+export default {
+render(h) {
+    return(
+        <div>
+        <Hello>
+        {{//作为插槽内容插入Hello里面
+            default: props => <button>{props.name}</button>
+        }}
+        </Hello>
+        </div>
+    )
+},
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue3+ts/vue3的Mixin和CompositionAPI/CompositionAPI/src/JSX组建的使用/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+setup(){
+
+}
+render(h) {
+    return (
+        <div>
+        <h2>hello,world</h2>
+        <div className='content'>
+        {//this.$slots.default拿到插槽
+            this.$slots.default ?this.$slots.default({name:'why'})
+         :<span>我是默认</span>
+        }
+        </div>
+        </div>
+    )
+},
+}
+</script>
+
+<style>
+
+</style>
