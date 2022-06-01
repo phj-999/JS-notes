5be0cd4f9cdac6ced4aa6f634e4cf2451e0fe66c
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-06-01 20:39:31 +0800

    如何实现Three.js中的模型世界轴旋转和自旋转

--- Js部分笔记/set.js
+++ Js部分笔记/set.js
@@ -1,5 +1,33 @@
 //数组去重
-const data = [1, 2, 3, , 4, 3, 3, 4];
+// const data = [1, 2, 3, , 4, 3, 3, 4];
 
-const newdata =[... new Set(data)]
-console.log(newdata,'new data')
+// const newdata =[... new Set(data)]
+// console.log(newdata,'new data')
+// for (var index = 0; index < 5; index++) {
+//     setTimeout(()=>console.log(index),1000) 
+// }
+// 5,5,5,5,5;   把var改成let就是），1，2，3，4
+
+// setTimeout(() => {
+//  console.log(1);   
+// })
+// new Promise((resolve, reject) => { 
+//     console.log(1);
+//     resolve(2)
+//     console.log(3);
+//  }).then((o) => console.log(0))
+
+//  new Promise((resolve, reject) => {
+//   console.log(4);
+//   resolve(5)  
+// }).then(o=>console.log(0)).then(()=>console.log(6))
+
+var scope = 'global scope'
+function checkscope() {
+    var scope = "local scope"
+    function f() {
+        return  console.log(scope);
+    }
+    return f
+}
+checkscope()()
