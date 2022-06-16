5ed8104059f870da49ec69ff5cac8da33c1817c8
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-06-16 12:38:52 +0800

    :sparkles:	搜索功能，给url拼接？后的参数Search Params，
    浏览器中的URL不仅由路径（本质上是一对段如和分隔符等）组成，而且还由一个可选的查询字符串（在React Router中称为搜索参数）组成，该字符串在URL中的分隔符之后以键/值对的形式出现。例如，将是一个具有一个搜索参数对的 URL，其中键为 ，值为 。以下示例将其显示为实现：http://localhost:3000/home#/users?name=Sarah+Finnley   name后的是输入的搜索参数

--- react-router-demo/mydemo/demo2/src/views/main-pages/users/Users.jsx
+++ react-router-demo/mydemo/demo2/src/views/main-pages/users/Users.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom';
+import React, { useCallback, useState } from "react";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
+import { Input } from "antd";
 
-const Users = ({users}) => {
-    return (
-      <main style={{ padding: '1rem 0' }}>
-        <h2>Users</h2>
-        <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {/* <Link to={`/users/${user.id}`}>
+const Users = ({ users }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchTerm = searchParams.get("name") || "";
+  const { Search } = Input;
+
+  // 搜索
+  const handleSearch = useCallback(
+    (value,event) => {
+      //const name = event.target.value;
+      const name = value;
+      if (name) {
+        setSearchParams({ name: value });
+      } else {
+        setSearchParams({});
+      }
+    },
+    [setSearchParams]
+  );
+
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Users</h2>
+      {/* 搜索 */}
+      <Search
+        placeholder="input search text"
+        enterButton="Search"
+        size="large"
+        onSearch={handleSearch}
+      />
+
+      <ul>
+        {users
+          .filter((user) =>
+            user.fullName.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+          )
+          .map((user) => (
+            <li key={user.id}>
+              {/* <Link to={`/users/${user.id}`}>
               {user.fullName}
             </Link> */}
-           <Link to={user.id}>
-              {user.fullName}
-            </Link>
-          </li>
-        ))}
+              <Link to={user.id}>{user.fullName}</Link>
+            </li>
+          ))}
       </ul>
       <Outlet />
-      </main>
-    );
-  };
+    </main>
+  );
+};
 
-export default Users
+export default Users;
